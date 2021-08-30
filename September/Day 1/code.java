47a6970b61cb0644b5ac9acd3af88c2a690045b5
saujanyamahajan <saujanya.9919@gmail.com>
2021-08-31 01:41:53 +0530

    updated code

--- September/Day 1/code.java
+++ September/Day 1/code.java
@@ -41,8 +41,76 @@ class Solution
         }
         return nge;
     } 
-}
+
 
 //2. Stock Span Problem
-//
+//https://practice.geeksforgeeks.org/problems/stock-span-problem-1587115621/1#
 
+public static int[] calculateSpan(int price[], int n){
+Stack<Integer> s=new Stack<>();
+int[] span=new int[n];
+
+span[0]=1;
+s.push(0);
+
+for(int i=1;i<n;i++)
+{
+    while(!s.empty() && price[i]>=price[s.peek()])
+    {
+        s.pop();
+    }
+    if(s.empty())
+    {
+        span[i]=i+1;
+    }
+    else
+    {
+        span[i]=i-s.peek();
+    }
+    s.push(i);
+}
+return span;
+}
+
+// 3.Smallest Number on left
+//https://practice.geeksforgeeks.org/problems/smallest-number-on-left3403/1
+
+
+static List<Integer> leftSmaller(int n, int a[])
+    {
+        //code here
+        Stack<Integer> s = new Stack<>();
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<=n-1;i++)
+        {
+            if(s.empty())
+            {
+                ans.add(-1);
+            }
+            else if(!s.empty() && s.peek()<a[i])
+            {
+             ans.add(s.peek());   
+            }
+            else if(!s.empty() && s.peek()>=a[i])
+            {
+              while(!s.empty()&& s.peek()>=a[i])
+              {
+                  s.pop();
+              }  
+              if(s.empty())
+              {
+                ans.add(-1);
+              }
+              else
+              {
+                 ans.add(s.peek());   
+              }
+            }
+        s.push(a[i]);
+        }
+        return ans;
+    }
+
+
+    
+}
