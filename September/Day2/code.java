c6b644ea07ae2408c087c19060f728350ec84511
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-01 21:20:08 +0530

    implement-stack using-queue

--- September/Day2/code.java
+++ September/Day2/code.java
@@ -137,4 +137,28 @@ class Solution{
     }
 
 
-}
+    //5.implement-stack-using-queue
+    //https://leetcode.com/problems/implement-stack-using-queues/submissions/
+    
+    private Queue<Integer> q = new LinkedList<>();
+
+
+    public void push(int x) {
+        q.add(x);
+        for(int i=1;i<q.size();i++){
+            q.add(q.remove());
+        }
+            
+        }
+    public int pop() {
+            return q.remove();
+
+    }
+    
+    public int top() {
+       return q.peek(); 
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
