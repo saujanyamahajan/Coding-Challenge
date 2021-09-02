64ed6f3f37aa179dbeb99cc57e63552aebc1de9b
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-02 17:30:08 +0530

    maxSlidingWindow

--- September/Day2/code.java
+++ September/Day2/code.java
@@ -262,4 +262,35 @@ class Solution{
       value = _value;
     }
   }
+
+
+  ///7.maxSlidingWindow
+  //https://leetcode.com/problems/sliding-window-maximum/
+  
+  public int[] maxSlidingWindow(int[] a, int k) {
+       
+        
+    int n = a.length;
+           int[] r = new int[n-k+1];
+           int ri = 0;
+           
+           Deque<Integer> q = new ArrayDeque<>();
+           for (int i = 0; i < a.length; i++) {
+               
+               if (!q.isEmpty() && q.peek() == i - k) {
+                   q.poll();
+               }
+               
+               while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
+                   q.pollLast();
+               }
+               
+               q.offer(i);
+               if (i >= k - 1) {
+                   r[ri++] = a[q.peek()];
+               }
+           }
+       return r;
+      
+       }
 }
