f007eb667e0f689f6e425417c941e2ad1dfa026c
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-07 23:57:42 +0530

    Check for Balanced Tree

--- September/Day 7/code.java
+++ September/Day 7/code.java
@@ -154,4 +154,44 @@ int height(Node node)
         return 1 + Math.max(rh,lh);
     }
 
+
+    //6.Check for Balanced Tree
+    //https://practice.geeksforgeeks.org/problems/check-for-balanced-tree/1#
+
+    boolean isBalanced(Node root)
+    {
+	// Your code here
+	if(dfsHeight(root) == -1)
+    {
+        return false;
+    }
+    return true;
+    }
+   
+    int dfsHeight (Node root)
+    {
+        if (root == null) 
+        {
+            return 0;
+        }    
+        
+        int leftHeight = dfsHeight (root.left);
+        if (leftHeight == -1)
+        {
+            return -1;
+        } 
+        
+        int rightHeight = dfsHeight (root.right);
+        if (rightHeight == -1)
+        { 
+            return -1;
+        }
+        
+        if (Math.abs(leftHeight - rightHeight) > 1)
+        {
+            return -1;
+        }    
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
 }
