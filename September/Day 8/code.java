7fa751958b9f7c4f21065486d37ea69374e60514
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-08 21:03:12 +0530

    Max rectangle

--- September/Day 8/code.java
+++ September/Day 8/code.java
@@ -26,4 +26,127 @@ public class code {
             return len;
         }
     
+
+
+        //2. Max rectangle 
+        //https://practice.geeksforgeeks.org/problems/max-rectangle/1#
+
+
+        public static int[] nsr(int[] arr){
+            int[] res = new int[arr.length]; 
+            Stack<Integer> st = new Stack<>();
+           // int x=(int)n;
+            //st.push(n-1);
+            
+            res[arr.length-1]=arr.length;
+            for(int i=arr.length-1;i>=0;i--)
+            {
+                if(st.empty())
+                {
+                   res[i]=arr.length; 
+                }
+                else if(!st.empty() && arr[i]>arr[st.peek()])
+                {
+                    res[i]=st.peek();                
+                }
+                else if(!st.empty() && arr[i]<=arr[st.peek()]){
+                    while(!st.empty() && arr[i]<=arr[st.peek()])
+                    {
+                        st.pop();
+                    }
+                    if(st.empty())
+                    {
+                        res[i]=arr.length;
+                    }
+                    else
+                    {
+                      res[i]=st.peek();  
+                    }
+            }
+                st.push(i);
+            }
+        return res;
+        }
+        
+         public static int[] nsl(int[] arr){
+            int[] res = new int[arr.length]; 
+            Stack<Integer> st = new Stack<>();
+            st.push(0);
+            res[0]=-1;
+            for(int i=1;i<arr.length;i++)
+            {
+                if(st.empty())
+                {
+                    res[i]=-1;
+                }
+                else if(!st.empty() && arr[i]>arr[st.peek()])
+                {
+                    res[i]=st.peek();                
+                }
+                else if(!st.empty() && arr[i]<=arr[st.peek()]){
+                    while(!st.empty() && arr[i]<=arr[st.peek()])
+                    {
+                        st.pop();
+                    }
+                    if(st.empty())
+                    {
+                        res[i]=-1;
+                    }
+                    else
+                    {
+                      res[i]=st.peek();  
+                    }
+                }
+                st.push(i);
+            }
+        return res;   
+        }
+        
+        public static int MAH(int h[]) 
+        {
+            int[] rs=new int[h.length];
+            int[] ls=new int[h.length];
+            int maxArea=0;
+            
+            rs=nsr(h);
+            ls=nsl(h);
+            
+            for( int i=0;i<h.length;i++)
+            {
+             int width=(rs[i]-ls[i])-1;
+             int area=h[i]*width;
+             if(area>maxArea)
+                {
+                    maxArea=area;
+                }
+            }
+            return maxArea;
+        }
+        
+        public int maxArea(int arr[][], int n, int m) {
+            int[] ans=new int[m];
+            for(int j=0;j<m;j++)
+            {
+                ans[j] = arr[0][j];
+            }
+            int maxi=MAH(ans);
+            
+            
+            for(int i=1;i<n;i++)
+            {
+                for(int j=0;j<m;j++)
+                {
+                    if(arr[i][j]==1){
+                        ans[j]++;
+                    }
+                    else
+                    {
+                        ans[j]=0;
+                    }
+                    //ans[j] = arr[0][j];
+                }
+            maxi=Math.max(maxi,MAH(ans));
+            }
+            return maxi;
+        }
 }
