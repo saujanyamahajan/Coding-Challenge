4487fea7773bbd533a7c9be16d075cd7d44ce4cc
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-08 23:50:45 +0530

    Maximum Path Sum in Binary Tree

--- September/Day 8/code.java
+++ September/Day 8/code.java
@@ -149,4 +149,26 @@ public class code {
             }
             return maxi;
         }
+
+
+
+        //3.Maximum Path Sum in Binary Tree
+        //https://leetcode.com/problems/binary-tree-maximum-path-sum/submissions/
+
+
+        public int maxPathSum(TreeNode root) {
+        
+            int maxValue[] = new int[1]; 
+                maxValue[0] = Integer.MIN_VALUE;
+                maxPathDown(root, maxValue);
+                return maxValue[0];
+            }
+            
+            private int maxPathDown(TreeNode node, int maxValue[]) {
+                if (node == null) return 0;
+                int left = Math.max(0, maxPathDown(node.left, maxValue));
+                int right = Math.max(0, maxPathDown(node.right, maxValue));
+                maxValue[0] = Math.max(maxValue[0], left + right + node.val);
+                return Math.max(left, right) + node.val;
+            }
 }
