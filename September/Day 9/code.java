dd561c3cc80c9f92f859d6bdf3b307522fcab606
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-10 01:24:39 +0530

    Combination Sum

--- September/Day 9/code.java
+++ September/Day 9/code.java
@@ -50,5 +50,39 @@ class Solution{
         }
         
     }
+
+
+    //3.Combination Sum
+    //https://leetcode.com/problems/combination-sum/submissions/
+
+
+
+    public List<List<Integer>> combinationSum(int[] arr, int B) {
+        List<List<Integer>> ans=new ArrayList<>();
+       find(0,arr,B,ans,new ArrayList<>());
+       return ans;
+   }
+   
+   
+   static void find(int idx,int[] arr, int B,List<List<Integer>> ans,List<Integer> ds)
+   {
+       if(idx==arr.length)
+       {
+           if(B==0)
+           {
+               ans.add(new ArrayList<>(ds));    
+           }    
+           return;
+       }
+       if(arr[idx]<=B)
+       {
+           ds.add(arr[idx]);
+           find(idx,arr,B-arr[idx],ans,ds);
+           ds.remove(ds.size()-1);
+           
+       }
+       find(idx+1,arr,B,ans,ds);
+   }
+   
     
 }
