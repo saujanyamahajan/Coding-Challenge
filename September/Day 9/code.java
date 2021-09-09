d91e30de632bdc08d57bdbe67234438f811e78de
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-09 22:51:45 +0530

    Unique Subsets

--- September/Day 9/code.java
+++ September/Day 9/code.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution{
 
     //1.Subset Sums
@@ -21,6 +23,32 @@ class Solution{
         
         find(idx+1,sum+arr.get(idx),arr,n,subsetsum);
         find(idx+1,sum,arr,n,subsetsum);
-    }    
+    }   
+    
+    
+    //2.Unique Subsets 
+    //https://leetcode.com/problems/subsets-ii/
+
+
+    public static ArrayList<ArrayList<Integer>> AllSubsets(int arr[], int n)
+    {
+        // your code here
+        Arrays.sort(arr);
+        ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
+        find(0,arr,new ArrayList<>(),ans);
+        return ans;
+    }
+    static void find(int idx,int arr[], ArrayList<Integer>ds,ArrayList<ArrayList<Integer>>ans)
+    {
+        ans.add(new ArrayList<>(ds));
+        for (int i=idx;i<arr.length;i++)
+        {
+            if(i!=idx && arr[i]==arr[i-1]) continue;
+            ds.add(arr[i]);
+            find(i+1,arr,ds,ans);
+            ds.remove(ds.size()-1);
+        }
+        
+    }
     
 }
