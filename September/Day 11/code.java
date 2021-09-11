33883e4b1d6b0e0dd6910782a87855432b189bcb
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-11 22:03:11 +0530

    Binary Tree Zigzag Level Order Traversal

--- September/Day 11/code.java
+++ September/Day 11/code.java
@@ -37,4 +37,85 @@ class Solution {
         }
         return ans;
     }
+
+
+
+    //2.Boundary Traversal of binary tree 
+    //https://practice.geeksforgeeks.org/problems/boundary-traversal-of-binary-tree/1
+
+
+    Boolean isLeaf(Node root)
+	{
+	    return (root.left==null) && (root.right==null)  ;
+	}
+	void addLeft(Node root,ArrayList<Integer>ans)
+	{
+	    Node curr=root.left;
+	    while(curr!=null)
+	    {
+	        if(isLeaf(curr)==false)
+	        {
+	            ans.add(curr.data);
+	        }   
+	        if(curr.left!=null)
+	        {
+	            curr=curr.left;
+	        }   
+	        else
+	        {
+	            curr=curr.right;
+	        }   
+	    }
+	}
+	void addRight(Node root,ArrayList<Integer>ans)
+	{
+	    Node curr=root.right;
+        ArrayList<Integer> temp=new ArrayList<>();
+	    while(curr!=null)
+	    {
+	        if(isLeaf(curr)==false)
+	        {
+	            temp.add(curr.data);
+	        }   
+	        if(curr.right!=null)
+	        {
+	            curr=curr.right;
+	        }   
+	        else
+	        {
+	            curr=curr.left;
+	        }   
+	    } 
+	    for(int i=temp.size()-1;i>=0;--i)
+	    {
+	        ans.add(temp.get(i));
+	    }
+	}
+	void addLeaf(Node root,ArrayList<Integer>ans)
+	{
+	    if(isLeaf(root))
+	    {
+	        ans.add(root.data);
+	        return;
+	    }    
+	    if(root.left!=null){
+	        addLeaf(root.left,ans);
+	    }    
+	    if(root.right!=null){
+	        addLeaf(root.right,ans);
+	    }    
+	        
+	}
+	ArrayList <Integer> printBoundary(Node node)
+	{
+        ArrayList<Integer> ans=new ArrayList<>();
+        if(isLeaf(node)==false){
+            ans.add(node.data);
+        }
+        addLeft(node,ans);
+        addLeaf(node,ans);
+        addRight(node,ans);
+        return ans;
+        
+	}
 }
