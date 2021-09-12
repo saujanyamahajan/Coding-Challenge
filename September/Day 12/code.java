27254892ed2411168834ca1201547c7f83348efe
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-13 00:47:26 +0530

    rotate a linked list

--- September/Day 12/code.java
+++ September/Day 12/code.java
@@ -103,4 +103,37 @@ class Solution {
         }
         return true;
     }    
+
+
+
+//3. rotate a linked list
+//https://practice.geeksforgeeks.org/problems/rotate-a-linked-list/1
+
+    public Node rotate(Node head, int k) {
+        // add code here
+        if(head==null || head.next==null || k==0)
+        {
+            return head;
+        }
+        
+        Node curr=head;
+        int len=1;
+        
+        while(curr.next!=null)
+        {
+            len++;
+            curr=curr.next;
+        }
+        
+        curr.next=head;
+        
+        while(k-->0)
+        {
+            curr=curr.next;
+            head=curr.next;
+        }
+        curr.next=null;
+        
+        return head;
+    }
 }
