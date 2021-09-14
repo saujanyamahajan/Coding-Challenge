31881441593b5cc42f269d364b261a5e80a14ff9
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-14 23:05:47 +0530

    Buy and Sell Stock

--- September/Day_14/code.java
+++ September/Day_14/code.java
@@ -67,4 +67,45 @@ public class code
     }
 
 
+
+    // 3. Best Time to Buy and Sell Stock
+
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+
+    public int maxProfit(int[] prices)
+    {
+        //BRUTE FORCE 
+//         int diff=0;
+//         int maxi=0;
+//         for(int i=0;i<prices.length;i++)
+//         {
+//             for(int j=i+1;j<prices.length;j++)
+//             {
+//                 diff=prices[j]-prices[i];
+//                 maxi=Math.max(maxi,diff);
+
+//             }
+//         }
+//         return maxi;
+        
+        //OPTIMAL
+        
+        int minprice=Integer.MAX_VALUE;
+        int maxprofit=0;
+        
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]<minprice)
+            {
+                minprice=prices[i];    
+            }   
+            else if(prices[i]-minprice>maxprofit)
+            {
+                maxprofit=prices[i]-minprice; 
+            }
+        }
+        return maxprofit;
+    }
+
 }
