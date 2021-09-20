104bf1733b99f89c34ac0366c6067847409664b1
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-20 15:02:51 +0530

    4. Median of Two Sorted Arrays

--- September/Day_20/code.java
+++ September/Day_20/code.java
@@ -49,6 +49,55 @@ public class code {
             }
         }
         
+
+        //4. Median of Two Sorted Arrays
+        //https://leetcode.com/problems/median-of-two-sorted-arrays/submissions/
+
+
+        public double findMedianSortedArrays(int[] num1, int[] num2) {
+            if(num2.length<num1.length)
+           {
+              return findMedianSortedArrays(num2,num1);
+           }
+           
+           int n1=num1.length;
+           int n2=num2.length;
+           
+           int low=0,high=n1;
+           
+           while(low<=high){
+               int cut1=(low+high)>>1;
+               int cut2=(n1+n2+1)/2-cut1;
+               
+               int l1=cut1==0?Integer.MIN_VALUE:num1[cut1-1];
+               int l2=cut2==0?Integer.MIN_VALUE:num2[cut2-1];
+               
+               int r1=cut1==n1?Integer.MAX_VALUE:num1[cut1];
+               int r2=cut2==n2?Integer.MAX_VALUE:num2[cut2];
+               
+               if(l1<=r2 && l2<=r1)
+               {
+                   if((n1+n2)%2==0)
+                   {
+                      return (Math.max(l1,l2)+Math.min(r1,r2))/2.0; 
+   
+                   }
+                   else
+                   {
+                      return Math.max(l1,l2); 
+                   }
+               }
+               else if(l1>r2)
+               {
+                   high=cut1-1; 
+               }
+               else
+               {
+                   low=cut1+1;
+               }   
+           }
+           return 0.0;
+       }
     }
     
     
