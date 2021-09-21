3e052d9b9f2b817997cd1068d510f6631b4446d9
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-21 14:30:57 +0530

    Maximize Number of 1's

--- September/Day_21/cide.java
+++ September/Day_21/cide.java
@@ -5,7 +5,21 @@ public class cide {
     //17. Maximum value of difference of a pair of elements and their Index 
     //https://practice.geeksforgeeks.org/problems/maximum-value-of-difference-of-a-pair-of-elements-and-their-index/1/?track=md-arrays&batchId=144
 
+    static int maxValue(int[] arr, int N) {
+        // code here
+        
+        
+        //brute force
+        // int ans = 0;
 
+        // for(int i = 0; i<N; i++)
+        // {
+        //     for(int j = i+1; j<N; j++)
+        //     {
+        //         ans = Math.max(ans, Math.abs(arr[i]-arr[j])+Math.abs(i-j));
+        //     }
+        // }
+        // return ans;
     int max1 = Integer.MIN_VALUE;
         int min1 = Integer.MAX_VALUE;
         int max2 = Integer.MIN_VALUE;
@@ -24,4 +38,40 @@ public class cide {
 
         return ans;
 
+}
+
+
+//14. Maximize Number of 1's 
+//https://practice.geeksforgeeks.org/problems/maximize-number-of-1s0905/1/?track=md-arrays&batchId=144
+
+
+    // m is maximum of number zeroes allowed to flip
+    int findZeroes(int arr[], int n, int m) {
+        // code here
+        int ans=0;
+        int j=-1;
+        int count=0;
+        
+        for(int i=0;i<n;i++) 
+        {
+            if(arr[i]==0)
+            {
+                count++;
+            }
+            while(count>m)
+            {
+                j++;
+                if(arr[j]==0)
+                {
+                    count--;
+                }
+            }
+            int len=i-j;
+            if(len>ans)
+            {
+                ans=len;
+            }
+        }
+                return ans;
+    }
 }
