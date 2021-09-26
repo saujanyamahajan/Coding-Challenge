820287607d3c8de19c2d6d7dc88d9c414486c921
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-26 21:27:27 +0530

    Look and Say Pattern

--- September/Day_26/code.java
+++ September/Day_26/code.java
@@ -71,4 +71,50 @@ public List<String> find_permutation(String S)
         }
         Collections.sort(stringList);
         return stringList;
+
+}
+
+
+//10. Look and Say Pattern 
+//https://practice.geeksforgeeks.org/problems/decode-the-pattern1138/1/?track=md-string&batchId=144
+
+
+static String lookandsay(int n) {
+    //your code here
+if (n == 1)     return "1";
+if (n == 2)     return "11";
+
+String str = "11";
+for (int i = 3; i <= n; i++)
+{
+   
+    str += '$';
+    int len = str.length();
+
+    int cnt = 1; 
+    String tmp = ""; 
+    char []arr = str.toCharArray();
+     
+   
+    for (int j = 1; j < len; j++)
+    {
+       
+        if (arr[j] != arr[j - 1])
+        {
+           
+            tmp += cnt + 0;
+            tmp += arr[j - 1];
+            cnt = 1;
+        }
+
+       
+        else cnt++;
+    }
+
+    str = tmp;
+}
+
+return str;
+
+}
 }
