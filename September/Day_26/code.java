0eeed3c13e8074538b5d586d60f56408aa62c3c1
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-26 19:58:26 +0530

    Permutations of a given string

--- September/Day_26/code.java
+++ September/Day_26/code.java
@@ -28,4 +28,47 @@ public static boolean isRotated(String str1, String str2)
     return (str1.equals(clock_rot)||str1.equals(anticlock_rot));
 }
 
+
+//16. Permutations of a given string 
+//https://practice.geeksforgeeks.org/problems/permutations-of-a-given-string2041/1/?track=md-string&batchId=144#
+
+public List<String> find_permutation(String S)
+    {
+        // Code here
+        int k=S.length();
+        int per=1;
+        while(k>0){
+           per*=k;
+           k--;
+        }
+        char[] array = S.toCharArray();
+        ArrayList<Character> charList = new ArrayList<>();
+        for(int i=0;i<array.length;i++){
+            charList.add(array[i]);
+        }
+        ArrayList<String> stringList=new ArrayList<>();
+        stringList.add(S);
+        while(stringList.size()!=per){
+    		Collections.shuffle(charList);
+    		for(int i=0;i<charList.size();i++){
+    		    array[i]=charList.get(i);
+    		}
+            String a=String.valueOf(array);
+            int flag=0;
+            int i=0;
+            while(flag==0 && i<stringList.size()){
+                if((stringList.get(i)).equals(a)){
+                    flag=1;
+                    break;
+                }
+                else{
+                    i++;
+                }
+            }
+            if(flag==0){
+                stringList.add(a);
+            }
+        }
+        Collections.sort(stringList);
+        return stringList;
 }
