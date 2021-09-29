d0929512982677e219948e68f7a02c24138fc9ed
saujanyamahajan <saujanya.9919@gmail.com>
2021-09-29 19:33:39 +0530

    Length of String

---
+++ September/Day_29/code.java
@@ -0,0 +1,20 @@
+class Solution
+{
+
+
+    //Length of String 
+    //https://practice.geeksforgeeks.org/problems/length-of-string/1
+
+
+    // Complete the function
+    // str: input string
+    public static int lengthString(String str)
+    {
+        // find the length of string
+        int ans=str.length();
+        return ans;
+    }
+
+
+
+}
