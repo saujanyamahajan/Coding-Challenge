be62087bf49883ee3f63684dda5169f355aed674
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-01 23:36:12 +0530

    Sort a stack

--- October/day1/code.java
+++ October/day1/code.java
@@ -1,3 +1,6 @@
+package October.day1;
+
+import java.util.*;
 public class code {
     
 
@@ -5,7 +8,6 @@ public class code {
     //https://practice.geeksforgeeks.org/problems/print-1-to-n-without-using-loops3621/1
 
 
-    class Solution{
         static void printTillN(int n){
             // code here
             
@@ -17,4 +19,37 @@ public class code {
             System.out.print(n+" ");
             
         }
+
+
+        //Sort a stack 
+        //https://practice.geeksforgeeks.org/problems/sort-a-stack/1
+
+
+        public void sortedInsert(Stack<Integer> s,int x)
+	{
+	    if (s.isEmpty()||x > s.peek())
+        {
+            s.push(x);
+            return;
+        }
+ 
+        int temp=s.pop();
+        sortedInsert(s, x);
+ 
+        s.push(temp);
+	}
+	
+	public Stack<Integer> sort(Stack<Integer> s)
+	{
+		//add code here.
+		if(s.size()==1)
+		{
+		    return s;
+		}
+		int x=s.pop();
+		sort(s);
+		sortedInsert(s,x);
+
+        return  s;
+	}
 }
