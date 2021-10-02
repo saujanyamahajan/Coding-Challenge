b9edcd9d50aef34033c16e653bf6c2ccb148b3ec
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-02 21:53:43 +0530

    Reverse a Stack using Recursion

--- October/day2/code.java
+++ October/day2/code.java
@@ -6,6 +6,7 @@ public class code
 
     //Delete Middle Element of a Stack
 //https://www.youtube.com/watch?v=oCcUNRMl7dA&list=PL_z_8CaSLPWeT1ffjiImo0sYTcnLzo-wY&index=8
+//https://practice.geeksforgeeks.org/problems/sort-a-stack/1
         public void solve(Stack<Integer> s,int k)
         {
             if (s.isEmpty()||k==1)
@@ -35,5 +36,44 @@ public class code
             return  s;
         }
 
+
+
+        //Reverse a Stack using Recursion
+        //https://www.youtube.com/watch?v=8YXQ68oHjAs&list=PL_z_8CaSLPWeT1ffjiImo0sYTcnLzo-wY&index=9
+
+
+
+        public void sortedInsert(Stack<Integer> s,int x)
+        {
+            if (s.isEmpty()||x > s.peek())
+            {
+                s.push(x);
+                return;
+            }
+     
+            int temp=s.pop();
+            sortedInsert(s, x);
+     
+            s.push(temp);
+        }
+        
+   
+        
+        public Stack<Integer> reverse(Stack<Integer> s)
+        {
+            //add code here.
+            if(s.size()==1)
+            {
+                return s;
+            }
+            int x=s.pop();
+    
+            reverse(s);
+            sortedInsert(s, x);
+    
+    
+            return  s;
+        }
+        
  }
     
