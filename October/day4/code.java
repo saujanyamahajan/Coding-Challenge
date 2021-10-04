13b7c59fc330ff72952d0ea29a72ed26c247718c
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-05 00:00:06 +0530

    key Pair

--- October/day4/code.java
+++ October/day4/code.java
@@ -20,5 +20,40 @@ public class code {
             }
             return dummy;
         }
+
+
+
+        //key Pair
+        //https://practice.geeksforgeeks.org/problems/key-pair1554/1/?track=md-hashing&batchId=144#
+
+
+        public boolean keypair(int[] A, int N, int X)
+    {
+        // code here
+        // int sum=0;
+        // HashSet<Integer> set=new HashSet<>();
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     sum=X-arr[i];
+             
+        //     if(set.contains(sum))
+        //     {
+        //         return true;
+        //     }
+        // }
+        // return false;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i<N; i++)
+        {
+            if(map.containsKey(X-A[i]))
+            {
+                return true;
+            }
+            map.put(A[i], i);
+        }
+        return false;
+    }
     } 
 
