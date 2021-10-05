2ff3a7556148ce1e00619f875c9ceda214b18cc1
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-05 21:27:15 +0530

    Letter Case Permutation

--- October/day5/code.java
+++ October/day5/code.java
@@ -35,5 +35,50 @@ public code
         Collections.sort(res);
         return res;        
     }
+
+
+    //784. Letter Case Permutation
+    //https://leetcode.com/problems/letter-case-permutation/
+
+    List<String> res;
+    public List<String> letterCasePermutation(String s)
+    {
+        res = new ArrayList<>();
+        solve(s,"",0);
+        //Collections.sort(res);
+        return res;
+    }
+    public void solve(String ip,String op,int n)
+    {
+        if(ip.length()==n)
+        {
+            res.add(op);
+            return;
+        }
+        String op1=new String(op);
+        String op2=new String(op);
+        char c=ip.charAt(n);
+        
+        if(Character.isAlphabetic(c))
+        {
+            op1=op1+c;
+            
+//             char ch=Character.toUpperCase(c);
+//             op2=op2+ch;
+            
+            c ^= (1 << 5); // converts lower-case to upper and upper to lower
+            op2=op2+c;
+
+            solve(ip,op1,n+1);
+            solve(ip,op2,n+1);
+        }
+        else
+        {
+            op1=op1+c;
+            solve(ip,op1,n+1);
+        }
+
+        return;
+    }
     
 }
