8cd6ab8427339ef508ffd8bdc18fac0057eee428
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-05 21:29:56 +0530

    Climbing Stairs

--- October/day5/code.java
+++ October/day5/code.java
@@ -80,5 +80,21 @@ public code
 
         return;
     }
+
+    //70. Climbing Stairs
+    //https://leetcode.com/problems/climbing-stairs/
+
+
+    public int climbStairs(int n) {
+        int[] dp=new int[n+1];
+         dp[0]=1;
+         dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+   
     
 }
