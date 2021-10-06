50c5f232560197cddb2940195faf4e13711fa40d
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-06 21:13:35 +0530

    Generate Parentheses

--- October/day6/code.java
+++ October/day6/code.java
@@ -79,3 +79,44 @@ class Solution {
         return res;
     }
 }
+
+
+//Generate Parentheses 
+//https://practice.geeksforgeeks.org/problems/generate-all-possible-parentheses/1#
+
+
+class Solution {
+    
+    public void solve(int open,int close,String op,List<String>res) 
+    {
+        if(open==0 && close==0)
+        {
+            res.add(op);
+            return ;
+        }
+        if(open!=0)
+        {
+            String op1=op+"(";
+           // res.add("(");
+            solve(open-1,close,op1,res);
+        }
+        if(close>open)
+        {
+            String op2=op+")";
+            //res.add(")");
+            solve(open,close-1,op2,res);
+        }
+        return;
+    }
+    public List<String> AllParenthesis(int n) 
+    {
+        // Write your code here
+        List<String> res=new ArrayList<>();
+        int open=n;
+        int close=n;
+        String op="";
+        
+        solve(open,close,op,res);
+        return res;
+    }
+}
