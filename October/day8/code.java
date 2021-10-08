3a6b81a505b3ba6f1b93750d4b9ee9ac1e0eb3fd
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-08 18:40:20 +0530

    Pascal's Triangle

--- October/day8/code.java
+++ October/day8/code.java
@@ -85,7 +85,33 @@ class Solution {
                  maxProfit=Math.max(maxProfit,price[i]-minPrice);
             }
             return maxProfit;
+        }   
+    }
+
+
+    //118. Pascal's Triangle
+    //https://leetcode.com/problems/pascals-triangle/
+
+
+    class Solution2 {
+        public List<List<Integer>> generate(int numRows)
+        {
+            List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+            if (numRows <=0){
+                return triangle;
+            }
+            for (int i=0; i<numRows; i++){
+                List<Integer> row =  new ArrayList<Integer>();
+                for (int j=0; j<i+1; j++){
+                    if (j==0 || j==i){
+                        row.add(1);
+                    } else {
+                        row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
+                    }
+                }
+                triangle.add(row);
+            }
+            return triangle;
+    
         }
-        
-        
     }
