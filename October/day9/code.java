aee425387bfaab408f169dd99f47cc5291ed5054
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-09 22:18:44 +0530

    Valid Sudoku

--- October/day9/code.java
+++ October/day9/code.java
@@ -112,3 +112,27 @@ class Solution3{
             return false;
     }
 }
+
+//36. Valid Sudoku
+//https://leetcode.com/problems/valid-sudoku/
+
+
+class Solution5 {
+    public boolean isValidSudoku(char[][] board)
+    {
+        Set occured = new HashSet();
+        for (int i=0; i<9; ++i)
+        {
+            for (int j=0; j<9; ++j) 
+            {
+                if (board[i][j] != '.')
+                {
+                    String b = "(" + board[i][j] + ")";
+                    if (!occured.add(b + i) || !occured.add(j + b) ||                                           !occured.add(i/3 + b + j/3))
+                        return false;
+                }
+            }
+        }
+    return true;
+    }
+}
