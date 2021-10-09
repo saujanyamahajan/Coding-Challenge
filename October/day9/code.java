8c4c312715fc386a0d490dd95c42e175b08e6ce6
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-09 22:16:49 +0530

    Word Search

--- October/day9/code.java
+++ October/day9/code.java
@@ -75,3 +75,40 @@ class Solution {
      return false;
     }
 }
+
+
+//79. Word Search
+//https://leetcode.com/problems/word-search/
+
+
+class Solution3{
+    public boolean exist(char[][] board, String word) 
+    {
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board[0].length; j++)
+                if (board[i][j] == word.charAt(0) && isFound(board, i, j, word, 0))
+                    return true;
+
+            return false;
+        }
+
+     private boolean isFound(char[][] board, int i, int j, String word, int index) {
+            if (index == word.length())
+                return true;
+            if (i < 0 || j < 0 || i >= board.length || j >= board[0].length)
+                return false;
+            if (word.charAt(index) != board[i][j])
+                return false;
+            char temp = board[i][j];
+            board[i][j] = '*';
+         
+         
+            if (isFound(board, i + 1, j, word, index + 1) ||
+                    isFound(board, i - 1, j, word, index + 1) ||
+                    isFound(board, i, j + 1, word, index + 1) ||
+                    isFound(board, i, j - 1, word, index + 1))
+                return true;
+            board[i][j] = temp;
+            return false;
+    }
+}
