b7519153b710cda91aa41101f27552f82497eab8
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-09 20:42:10 +0530

    Search a 2D Matrix

--- October/day9/code.java
+++ October/day9/code.java
@@ -55,3 +55,23 @@ class Trie {
             return true;
     }
 }
+
+
+
+//74. Search a 2D Matrix
+//https://leetcode.com/problems/search-a-2d-matrix/
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target)
+    {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                 if(target==matrix[i][j]){
+                return true;
+                }
+            }
+        } 
+     return false;
+    }
+}
