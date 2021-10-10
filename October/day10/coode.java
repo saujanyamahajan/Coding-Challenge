c9a579b770d1cae9a5b0d38ef90f06550958402d
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-10 20:07:57 +0530

    First Unique Character in a String

---
+++ October/day10/coode.java
@@ -0,0 +1,26 @@
+package October.day10;
+
+
+
+//387. First Unique Character in a String
+//https://leetcode.com/problems/first-unique-character-in-a-string/
+
+
+class Solution {
+    public int firstUniqChar(String s)
+    {
+        int[] freq=new int[26];
+        for(char c :s.toCharArray())
+        {
+            freq[c-'a']++;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if(freq[s.charAt(i)-'a']==1)
+            {
+                return i;    
+            }
+        }
+        return -1;
+    }
+}
