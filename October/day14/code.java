6f3ad98e1e2f5b58cfce9b4918b5adddfa7ee405
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-14 22:51:55 +0530

    Binary Tree Inorder Traversal

--- October/day14/code.java
+++ October/day14/code.java
@@ -81,3 +81,42 @@ class Solution {
         return ans;
     }
 }
+
+
+// Binary Tree Inorder Traversal
+//https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        
+        if(root==null)
+        {
+            return ans;
+        }
+        
+        Stack<TreeNode>st=new Stack<>();
+        TreeNode node=root;
+        
+        while(true)
+        {
+            if(node!=null)
+            {
+                st.push(node);
+                node=node.left;
+            }
+            else
+            {
+                if(st.isEmpty())
+                {
+                    break;
+                }
+                node=st.pop();
+                ans.add(node.val);
+                node=node.right;
+            }
+        }
+        return ans;
+    }
+}
