f8eaa1f85e2fcaddb6e43c560839e22ced5ad2c1
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-14 22:45:47 +0530

    Binary Tree Preorder Traversal

--- October/day14/code.java
+++ October/day14/code.java
@@ -50,3 +50,34 @@ class MyQueue {
         return op.empty() && ip.empty();
     }
 }
+
+
+
+//144. Binary Tree Preorder Traversal
+//https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+        ArrayList<Integer> ans=new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Stack<TreeNode>st=new Stack<>();
+        st.push(root);
+        
+        while(!st.isEmpty()){
+            root=st.pop();
+            ans.add(root.val);
+           
+            if(root.right!=null){
+                st.push(root.right);
+            }
+             if(root.left!=null){
+                st.push(root.left);
+            }
+        }
+        return ans;
+    }
+}
