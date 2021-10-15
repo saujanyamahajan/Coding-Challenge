defe1ce3080ac597bd819865b6c3a26c5f51f516
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-15 20:11:05 +0530

    Maximum Depth of Binary Tree

--- October/day15/code.java
+++ October/day15/code.java
@@ -53,3 +53,20 @@ class Solution2 {
 		return res;
     }
 }
+
+
+
+//104. Maximum Depth of Binary Tree
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+
+class Solution3 {
+    public int maxDepth(TreeNode root)
+    {
+         if(root==null)
+         {
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+}
