569e1080daf2e9378ea6751e90159a715e8835c8
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-17 21:20:56 +0530

    Path Sum III

--- October/day16/code.java
+++ October/day16/code.java
@@ -1,10 +1,11 @@
-
+java.util.*;
+package October.day16;
 
 //112. Path Sum
 //https://leetcode.com/problems/path-sum/
 
 
-class Solution {
+class Solution1{
     public boolean hasPathSum(TreeNode root, int sum)
     {
         if (root == null)
@@ -36,5 +37,45 @@ class Solution2 {
         root.left = invertTree(root.right);
         root.right = invertTree(tmp);
         return root;    
+    }
+}
+
+
+//437. Path Sum III
+//https://leetcode.com/problems/path-sum-iii/
+
+
+class Solution {
+    public int pathSum(TreeNode root, int targetSum)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        
+        ArrayList<Integer> ans=new ArrayList<>();
+        int[]count=new int[1];
+        helper(root,targetSum,count,ans);
+        return count[0];
+    }
+    public void helper(TreeNode root,int targetSum,int[] count,ArrayList<Integer>       ans)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        ans.add(root.val);
+        int sum=0;
+        for(int i=ans.size()-1;i>=0;i--)
+        {
+            sum+=ans.get(i);
+            if(sum==targetSum)
+            {
+                count[0]++;
+            }
+        }
+        helper(root.left,targetSum,count,ans);
+        helper(root.right,targetSum,count,ans);
+        ans.remove(ans.size()-1);
     }
 }
