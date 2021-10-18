e2eb026fd4b9733a2b6d74058ea69a03cb0f825b
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-19 00:16:28 +0530

    Combination Sum II

--- October/day18/code.java
+++ October/day18/code.java
@@ -35,3 +35,36 @@ class Solution {
     }
     
 }
+
+
+//40. Combination Sum II
+//https://leetcode.com/problems/combination-sum-ii/
+
+
+class Solution1 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) 
+    {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates); 
+        combinations(0, candidates, target, ans, new ArrayList<>()); 
+        return ans; 
+    }
+    private void combinations(int idx, int[] arr, int target,                               List<List<Integer>>ans, List<Integer> ds)
+    {
+        if(target == 0)
+        {
+            ans.add(new ArrayList<>(ds)); 
+            return; 
+        }
+        
+        for(int i = idx; i < arr.length;i++)
+        {
+            if(i > idx && arr[i] == arr[i-1]) continue; 
+            if(arr[i]>target) break; 
+            
+            ds.add(arr[i]); 
+            combinations(i+1, arr, target - arr[i], ans, ds); 
+            ds.remove(ds.size() - 1); 
+        }
+    }
+}
