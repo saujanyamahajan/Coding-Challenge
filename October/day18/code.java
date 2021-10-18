e73fad06243b0069204725eafe957a8251409a73
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-19 01:58:47 +0530

    Palindrome Partitioning

--- October/day18/code.java
+++ October/day18/code.java
@@ -68,3 +68,45 @@ class Solution1 {
         }
     }
 }
+
+
+//131. Palindrome Partitioning
+//https://leetcode.com/problems/palindrome-partitioning/
+
+
+class Solution3 {
+    public List<List<String>> partition(String s) 
+    {
+        List<List<String>> res=new ArrayList<>();
+        List<String> path=new ArrayList<>();
+        find(0,s,path,res);
+        return res;
+    }
+    
+    void find(int idx,String s,List<String> path,List<List<String>>res)
+    {
+        if(idx==s.length())
+        {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i=idx;i<s.length();++i)
+        {
+            if(isPalindrome(s,idx,i))
+            {
+                path.add(s.substring(idx,i+1));
+                find(i+1,s,path,res);
+                path.remove(path.size()-1);
+            }
+        }
+    }
+    boolean isPalindrome(String s,int start,int end)
+    {
+        while(end>=start)
+        {
+            if(s.charAt(start++)!=s.charAt(end--))
+                return false;
+        }
+        return true;
+    }
+}
