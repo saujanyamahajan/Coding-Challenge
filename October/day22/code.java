621833fef18cf2b75f6ecad588c9b663ea513560
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-23 13:00:56 +0530

    Tower of Hanoi

--- October/day22/code.java
+++ October/day22/code.java
@@ -18,3 +18,28 @@ class Solution
 }
 
 
+//4. Tower Of Hanoi 
+//https://practice.geeksforgeeks.org/problems/tower-of-hanoi-1587115621/1/?track=md-recursion&batchId=144#
+
+
+class Hanoi {
+
+    long count=0;
+    
+    public long toh(int n, int from, int to, int aux)
+    {
+        // Your code here
+        if (n == 1)
+        {
+            System.out.println("move disk "+n+" from rod "+from+" to rod "+to);
+            count++;
+            return count;
+        }
+        
+        toh(n - 1, from, aux, to);
+        System.out.println("move disk "+n+" from rod "+from+" to rod "+to);
+        count++;
+        return toh(n - 1, aux, to, from);
+        
+    }
+}
