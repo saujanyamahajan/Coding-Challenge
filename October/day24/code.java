f5da357a55020c735e387b7659ba9818424aad79
saujanyamahajan <saujanya.9919@gmail.com>
2021-10-24 18:59:47 +0530

    Binary Search

---
+++ October/day24/code.java
@@ -0,0 +1,30 @@
+
+//704. Binary Search
+//https://leetcode.com/problems/binary-search/
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0;
+        int end = nums.length-1;
+        
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                end=mid-1;
+                
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+}
